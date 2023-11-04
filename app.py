b7190e21aea39251405cdc3a4b23198649116b6a
atuko <66684753+atuko315@users.noreply.github.com>
2023-11-04 18:14:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def update_board():
     updated_board = data['board']  # クライアントからのデータを受け取り
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
