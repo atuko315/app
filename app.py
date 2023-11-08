aef0770542af76331c17c9111c2131f94f55ce92
atuko <66684753+atuko315@users.noreply.github.com>
2023-11-08 22:21:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -639,4 +639,6 @@ def detect_relative_distance(pa, ca, limit=3):
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
